3936799e6aee7c0ebbe5d97aa64b58ce36ed3520
Eric Ho <endonoho@gmail.com>
2020-07-31 01:05:43 -0700

    Animate modal

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Modal = ({ selectedImg, setSelectedImg }) => {
   // close the modal
@@ -12,9 +13,18 @@ const Modal = ({ selectedImg, setSelectedImg }) => {
   return (
     // the backdrop behind modal (semi transparent sheet that fades behind the modal)
     // BUG FIXED: we only want to click on this div and not the image to close
-    <div className="backdrop" onClick={handleClick}>
-      <img src={selectedImg} alt="enlarged pic" />
-    </div>
+    <motion.div className="backdrop" onClick={handleClick}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <motion.img src={selectedImg} alt="enlarged pic"
+      // offset for y axis
+      // takes it above the browser screen
+        initial={{ y: "-100vh" }}
+      // bring back to original position
+        animate={{ y: 0 }}
+      />
+    </motion.div>
   )
 
 }
