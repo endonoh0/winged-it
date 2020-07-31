14faf2541f816b42fd6245165ffd4f5fdeec6125
Eric Ho <endonoho@gmail.com>
2020-07-31 00:52:56 -0700

    Close modal when backdrop is clicked

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -3,12 +3,15 @@ import React from 'react';
 const Modal = ({ selectedImg, setSelectedImg }) => {
   // close the modal
   const handleClick = (e) => {
-    setSelectedImg(null);
+
+    if (e.target.classList.contains('backdrop')) {
+      setSelectedImg(null);
+    }
   }
 
   return (
     // the backdrop behind modal (semi transparent sheet that fades behind the modal)
-    // BUG: we only want to click on this div and not the image to close
+    // BUG FIXED: we only want to click on this div and not the image to close
     <div className="backdrop" onClick={handleClick}>
       <img src={selectedImg} alt="enlarged pic" />
     </div>
