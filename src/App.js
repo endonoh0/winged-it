42001fc227f77a6de5d0ec4b1cd502d0731da0fb
Eric Ho <endonoho@gmail.com>
2020-07-31 00:48:06 -0700

    Open selected image in modal and close it when clicked again

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Title/>
       <UploadForm />
       <ImageGrid setSelectedImg={setSelectedImg} />
-      <Modal selectedImg={selectedImg}/>
+      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} /> }
     </div>
   );
 }
--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Modal = ({ selectedImg }) => {
+const Modal = ({ selectedImg, setSelectedImg }) => {
+  // close the modal
+  const handleClick = (e) => {
+    setSelectedImg(null);
+  }
 
   return (
     // the backdrop behind modal (semi transparent sheet that fades behind the modal)
-    <div className="backdrop">
+    <div className="backdrop" onClick={handleClick}>
       <img src={selectedImg} alt="enlarged pic" />
     </div>
   )
