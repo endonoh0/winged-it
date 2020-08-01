643dc3b978a5d9a38f5a4a1945d9bf6d571aebdc
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-08-01 02:39:31 -0600

    fixed the api side

--- src/helper/foodApi.js
+++ src/helper/foodApi.js
@@ -1,7 +1,7 @@
 const requestPromise = require('request-promise');
 
 // this function receive ingredients in an array and return the promise with api search results
-const apiSearch = (ingredients) => {
+const recipeSearch = (ingredients) => {
 
   const apiInfo = {
     edamamRecipeDatabase: {
@@ -9,27 +9,25 @@ const apiSearch = (ingredients) => {
       applicationID: "0981a1a2b74f3547b529351748da57c2"
     }
   };
+ 
 
   const edamamRecipeUrl = 
   `https://api.edamam.com/search?q=${ingredients}&app_id=${apiInfo.edamamRecipeDatabase.apiKey}&app_key=${apiInfo.edamamRecipeDatabase.applicationID}`;
   
-  ingredientString = ingredients.join('%20');
 
   return requestPromise(edamamRecipeUrl)
   .then (function (data) {
     
     // this return 10 recipes in an array and if it can not find anything, return null
-    parsedData = JSON.parse(data);
-    console.log(parsedData.hits);
+    const parsedData = JSON.parse(data);
     return parsedData.hits;
 
   })
   .catch(err => {
-    return err;
+    console.log(err);
   })
 
 };
 
-module.exports = {apiSearch}
 
-
+export default recipeSearch;
