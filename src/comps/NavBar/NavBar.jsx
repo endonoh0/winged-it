c140f91b6f0e33807c55b05bea92b43b509efa2c
Brian Tran <98brian.tran@gmail.com>
2020-08-01 11:14:54 -0700

    Basic routing for singup signin and fovorties

--- src/comps/NavBar/NavBar.jsx
+++ src/comps/NavBar/NavBar.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './NavBar.scss'
 
 const NavBar = () => {
 	return(
 		<section className="sidebar">
-			<h1>Winged It</h1>
+			<Link to='/' className="link"><h1>Winged It</h1></Link>
 			<hr className="sidebar__separator sidebar--centered" />
 			<nav className="sidebar__menu">
+				<ul>
+					<li>
+						<Link to='signup' className='link'>
+							Sign Up
+						</Link>
+					</li>
+					<li>
+						<Link to='signin' className='link'>
+							Sign In
+						</Link>
+					</li>
+					<li>
+						<Link to='favorites' className='link'>
+							Favorites
+						</Link>
+					</li>
+				</ul>
 			</nav>
 		</section>
 	);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,31 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import NavBar from './comps/NavBar/NavBar'
+import SignUp from './comps/SignUp'
+import SignIn from './comps/SignIn'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <div className="container">
-      <NavBar />
-      <App />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route path="/">
+            <App />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
