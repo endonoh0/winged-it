71a2938540f7aef1d3663afc561c2662ae9b5397
Brian Tran <98brian.tran@gmail.com>
2020-08-04 11:46:52 -0700

    Changed hook to work with being insde of a callback

--- src/hooks/useWriteToFirestore.js
+++ src/hooks/useWriteToFirestore.js
@@ -1,18 +1,30 @@
 import { useEffect } from 'react'
 import { onAuthStateChange, projectFirestore } from '../firebase/config'
 
-const useWriteToFirestore = (collection, dependancy, data) => {
-	useEffect(() => {
-    
-    onAuthStateChange(user => {
+const useWriteToFirestore = () => {
+	// useEffect(() => {
+  //   onAuthStateChange(user => {
+	// 		// Makes sure that a user is logged in to perform this function
+  //     if(user && data) {
+  //       projectFirestore.collection(collection)
+	// 			.doc(user.uid) // Creates a document in firebase and names it after user's UID
+	// 			.set(data) // An object that will be added to DB under the user uid
+  //     }
+  //   })
+	// },[dependancy, collection, data])
+	
+	const write = (collection, dependancy, data) => {
+		onAuthStateChange(user => {
 			// Makes sure that a user is logged in to perform this function
-      if(user) {
+      if(user && data) {
         projectFirestore.collection(collection)
 				.doc(user.uid) // Creates a document in firebase and names it after user's UID
 				.set(data) // An object that will be added to DB under the user uid
       }
     })
-  },[dependancy, collection, data])
+	}
+
+	return {write}
 }
 
 export default useWriteToFirestore
