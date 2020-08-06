ac4b511bcc21d9153e45c5b67f47c700949bb3f9
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-08-05 22:04:21 -0600

    styled the tags

--- src/comps/SearchByIngredient/SearchTag.jsx
+++ src/comps/SearchByIngredient/SearchTag.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './SearchTag.scss'
 
+import { RiDeleteBin5Line } from 'react-icons/ri';
+
 const SearchTag = ({searchTags, removeTag}) => {
   const [tags, setTags] = useState([]);
   
@@ -8,17 +10,25 @@ const SearchTag = ({searchTags, removeTag}) => {
     setTags(searchTags);
   },[searchTags]);
 
+  const firstLetterToUpperCase = (str) => {
+    
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  } 
+
+
 	return (
     <div className='tag-container'>
       {tags.map((tag, index) => 
-      <div className='search-tag'key={index}>
-        <div>
-          {tag}
-          
+      <div className='search-tag' key={index}>
+        <div >
+          {firstLetterToUpperCase(tag)}
         </div>
-        <span className='remove' onClick={e => removeTag(tag)}>x</span>
+        <span className='remove' onClick={e => removeTag(tag)}><RiDeleteBin5Line/></span>
+        
       </div>
+      
       )}
+
     </div>
   )
 }
