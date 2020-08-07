355f23923b980ed78cc132cc08397dd99be415f2
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-08-07 13:03:43 -0600

    refractored the code

--- src/comps/Home/Home.jsx
+++ src/comps/Home/Home.jsx
@@ -13,34 +13,32 @@ const Home = () => {
             <h3> Logo</h3>
             <div className="home_top_bar_left">
               <div>
-              <a id="search_recipe" className="home_top_bar_left_link" href="/">Search Recipe</a>
+                <a id="search_recipe" className="home_top_bar_left_link" href="/">Search Recipe</a>
               </div>
               <div>
-              <a className="home_top_bar_left_link" href="/map">Farmer Markets</a>
+                <a className="home_top_bar_left_link" href="/map">Farmer Markets</a>
               </div>
               <div>
-              <a className="home_top_bar_left_link" href="/signup">Sign up</a>
+                <a className="home_top_bar_left_link" href="/signup">Sign up</a>
               </div>
               <div>
-              <a className="home_top_bar_left_link" href="/signin">Sign in</a>
+                <a className="home_top_bar_left_link" href="/signin">Sign in</a>
               </div>
             </div>
           </div>
-
           <div className="centered">
             <div className="title_home_page">
               <span className="horizontal_line"> </span>
               <div className="title_text_home_page"> Winged It</div>
               <span className="horizontal_line"></span>
             </div>
-
             <div className="text_description">Eat Fresh - Eat Healthy </div>
             <div className="home_buttons">
               <a className="home_button home_button_left" href='/'>
-              Search Recipe
+                Search Recipe
               </a>
               <a className="home_button"  href='/map'>
-              Farmer Markets
+                Farmer Markets
               </a>
             </div>
           </div>
--- src/comps/Home/HomeBottomSection.jsx
+++ src/comps/Home/HomeBottomSection.jsx
@@ -7,10 +7,8 @@ const HomeBottomSection = () => {
   return (
     <div>
       <div className="home_div_botton">
-      <div className="img_container">
-      
+        <div className="img_container">
       <figure className="home_figure ">
-      
       <img className="gallery__img img_left static" src="gallery__img img_right static" src="./bread.png"/>
       <img className="gallery__img img_left active" src="gallery__img img_right static" src="./bread.gif"></img>
       
@@ -29,14 +27,12 @@ const HomeBottomSection = () => {
       </div>
       
       </div>
-      <div className="home_desription">
-      <h3  className="home_desription_title">Winged It</h3>
-      <p className="home_desription_text">Whether you're looking for healthy recipes, or ideas on how to use leftovers from your fridge, we've numerous recipes to choose from, so you'll be able to find the perfect dish.</p>
+        <div className="home_desription">
+          <h3  className="home_desription_title">Winged It</h3>
+          <p className="home_desription_text">Whether you're looking for healthy recipes, or ideas on how to use leftovers from your fridge, we've numerous recipes to choose from, so you'll be able to find the perfect dish.</p>
+        </div>
       </div>
-      
-      </div>
-        <div id="bottom_bar_home">
-      </div>
+      <div id="bottom_bar_home"></div>
     </div>
     );
   }
