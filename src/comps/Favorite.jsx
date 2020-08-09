d8fa78e7e0549eace58c05b012794bb8f66237da
brian tran <98brian.tran@gmail.com>
2020-08-09 15:51:48 -0700

    Trying to redesign the favorite page with elements from the old fav and the ingredients page

--- src/comps/Favorite.jsx
+++ src/comps/Favorite.jsx
@@ -5,6 +5,7 @@ import { useVisualMode } from '../hooks/useVisualMode';
 import { useFirestoreFavorites } from '../hooks/useFirestoreFavorites'
 import { projectFirestore } from '../firebase/config';
 
+import Card from  'react-bootstrap/Card'
 import FavoritePage from './Favorite/index';
 import Loading from './Favorite/Loading';
 import Empty from './Favorite/Empty';
@@ -75,56 +76,104 @@ const Favorite = ({setSelectedImg, user}) => {
 
     transition(SHOW);
   }
-
-  return (
-    <div className="favorite__container">
-      { mode === EDIT && 
-        <Fragment>
-          <TitleFav message= "Edit Your Recipe" />
-          <Edit
-          onSave={ save }
-          onCancel={ back }
-          editPlaceholder = { favItems[editDoc[0]].recipe.name } />
-        </Fragment>
-        }
-      { mode === EMPTY && <div>
-        <TitleFav message= "Favorite List" />
-        <Empty/>
-      </div> }
-      { mode === LOADING && 
-        <div>
-          <TitleFav message= "Favorite List" />
-          <Loading/>
-        </div>
-         }
-      { mode === SHOW &&
-      <Fragment>
-        <TitleFav message= "Favorite List" />
-        <div className="img-grid">
-          {
-            favItems.map((favItem, index) => {
-              return  <motion.div  className="img-wrap"
-               key = { favItem.id }
-                layout
-                whileHover={{ opacity: 1 }}
-                // onClick={() => setSelectedImg(favItem.recipe.url)}
-                >
-                <FavoritePage 
+	return (
+		<Fragment>
+			<div className="seasonal_banner">
+				<div className="seasonal_revealer">
+					<h1 className="banner_content" id="seasonal_title">Favorites</h1>
+					<article className="banner_description">
+						{/* <h2 className="banner_content" id="month">{MONTHS[month]}</h2> */}
+						<p className="banner_content" id="description">
+							Re-explore your favorite meals and remember why you fell in love with these recipes in the first place!
+						</p> 
+					</article>
+				</div>
+				<img className="banner_left" src="./ingredient-banner.jpg" alt="Ingredient banner"/>
+			</div>
+			<section className="seasonal_container">
+				<article className="grids_container">
+					{mode === LOADING &&
+						<Loading />
+					}
+          {mode === EDIT && 
+            <Fragment>
+              <TitleFav message= "Edit Your Recipe" />
+              <Edit
+              onSave={ save }
+              onCancel={ back }
+              editPlaceholder = { favItems[editDoc[0]].recipe.name } />
+            </Fragment>
+          }
+					{mode === SHOW &&
+						favItems.map((favItem, index) => {
+              // <Link className="ingredient" to="/">
+              return(
+								<Card className="grid__card" style ={{width: '18rem'}}>
+                                  <FavoritePage 
                 doc = { favItem }
                 setSelectedImg = {setSelectedImg}
                 deleteEvent = { e => deleteEvent(index, favItem.id)}
                 editEvent = { e => editEvent(index, favItem.id) }/> 
-              </motion.div>
+									{/* <Card.Img className="grid__img" variant="top" src={favItem.recipe.img} />
+									<Card.Title className="grid__title" >{favItem.recipe.label}</Card.Title> */}
+								</Card>
+              // </Link>
+              )
+              })}
+				</article>
+			</section>
+    </Fragment>)
+    
+
+  // return (
+  //   <div className="favorite__container">
+  //     { mode === EDIT && 
+  //       <Fragment>
+  //         <TitleFav message= "Edit Your Recipe" />
+  //         <Edit
+  //         onSave={ save }
+  //         onCancel={ back }
+  //         editPlaceholder = { favItems[editDoc[0]].recipe.name } />
+  //       </Fragment>
+  //       }
+  //     { mode === EMPTY && <div>
+  //       <TitleFav message= "Favorite List" />
+  //       <Empty/>
+  //     </div> }
+  //     { mode === LOADING && 
+  //       <div>
+  //         <TitleFav message= "Favorite List" />
+  //         <Loading/>
+  //       </div>
+  //        }
+  //     { mode === SHOW &&
+  //     <Fragment>
+  //       <TitleFav message= "Favorite List" />
+  //       <div className="img-grid">
+  //         {
+  //           favItems.map((favItem, index) => {
+  //             return  <motion.div  className="img-wrap"
+  //              key = { favItem.id }
+  //               layout
+  //               whileHover={{ opacity: 1 }}
+  //               // onClick={() => setSelectedImg(favItem.recipe.url)}
+  //               >
+  //               <FavoritePage 
+  //               doc = { favItem }
+  //               setSelectedImg = {setSelectedImg}
+  //               deleteEvent = { e => deleteEvent(index, favItem.id)}
+  //               editEvent = { e => editEvent(index, favItem.id) }/> 
+  //             </motion.div>
               
-            })
-          }
-        </div>
-      </Fragment>
+  //           })
+  //         }
+  //       </div>
+  //     </Fragment>
     
-    }
-    </div>
+  //   }
+  //   </div>
     
-  );
+  // );
 }
 
 export default Favorite;
