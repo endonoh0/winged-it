4738af4202d667031ddd75185c892bbb454da8d4
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-08-11 21:48:31 -0600

    updated the description

--- src/comps/Home/BottomSection/BottomSection.jsx
+++ src/comps/Home/BottomSection/BottomSection.jsx
@@ -12,7 +12,7 @@ const BottomSection = () => {
           <span className="horizontal_line"> </span>
         </div>
         <div className="home_text">
-          <p className="home_desription_text">Whether you're looking for healthy recipes, or ideas on how to use leftovers from your fridge, we've numerous recipes to choose from, so you'll be able to find the perfect dish.</p>
+          <p className="home_desription_text">Whether you're looking for healthy recipes,  or ideas on how to use leftovers from your fridge, we've numerous recipes to choose from. You can browse on-season vegetables/fruits and see the closest local farmer market to you.  </p>
         </div>
 
         <div className="img_container">
